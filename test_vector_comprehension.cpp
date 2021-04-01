abe7dcae9aa8f99ea87214344b7441b7c266ed3e
Patricio Loncomilla <ploncomi@gmail.com>
2021-04-01 15:30:45 -0300

    Add files via upload

--- test_vector_comprehension.cpp
+++ test_vector_comprehension.cpp
@@ -2,13 +2,17 @@
 #include <string>
 #include "vector_comprehension.h"
 
-
-template<class T>
-void print_vector(const std::vector<T> &arr)
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 {
-  for (const T& x : arr)
-    std::cout << x << ", ";
-  std::cout << std::endl;
+    os << "[";
+    for (int i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    os << "]";
+    return os;
 }
 
 int main(void)
@@ -16,11 +20,11 @@ int main(void)
   using namespace std;
   std::vector<int> vals = {6,7,8,9,10};
 
-  print_vector( VECTCOMP(2*x, int x, vals) );
-  print_vector( VECTCOMP(2*x, int x, {1,2,3,4,5}) );
-  print_vector( VECTCOMP("Numero "+to_string(x), int x, vals) );
-  print_vector( VECTCOMP(x, int x, vals) );
-  print_vector( VECTCOMPIF(x, int x, vals, if (x%2 == 0)) );
+  cout << VECTCOMP(2*x, int x, vals) << endl;
+  cout << VECTCOMP(2*x, int x, {1,2,3,4,5}) << endl;
+  cout << VECTCOMP("Numero "+to_string(x), int x, vals) << endl;
+  cout << VECTCOMP(x, int x, vals) << endl;
+  cout << VECTCOMPIF(x, int x, vals, if (x%2 == 0)) << endl;
 
   return 0;
 }
